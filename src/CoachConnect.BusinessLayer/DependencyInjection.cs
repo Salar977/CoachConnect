389726f460ec414427b6f7b0d29420ce81dce9a0
Salar Amir <salar831@gmail.com>
2024-02-08 14:51:01 +0100

    Add project files.

---
+++ src/CoachConnect.BusinessLayer/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CoachConnect.BusinessLayer;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        
+
+
+        return services;
+    }
+}

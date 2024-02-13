c0bfb7159c3432222f2ad727e7056f92f508012b
KiwiKetil <ketilsveberg@gmail.com>
2024-02-13 10:30:30 +0100

    made ready for push

--- src/CoachConnect.BusinessLayer/DependencyInjection.cs
+++ src/CoachConnect.BusinessLayer/DependencyInjection.cs
@@ -8,8 +8,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddServices(this IServiceCollection services)
     {
-        services.AddScoped<SerilogTestService>();
-        services.AddScoped<SerilogTestDataAccessLayer>();
+        
 
         return services;
     }

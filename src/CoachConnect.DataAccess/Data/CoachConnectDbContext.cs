3bc6f82f3876c6a7cbae8399107cc9d7fda5aab7
KiwiKetil <122778291+KiwiKetil@users.noreply.github.com>
2024-02-14 13:36:31 +0100

    added CoachConnectDbContext (#5)

--- src/CoachConnect.DataAccess/Data/CoachConnectDbContext.cs
+++ src/CoachConnect.DataAccess/Data/CoachConnectDbContext.cs
@@ -7,7 +7,15 @@ public class CoachConnectDbContext : DbContext
 {
     public CoachConnectDbContext(DbContextOptions<CoachConnectDbContext> options) : base(options)
     {
-        
+
     }
 
+    public DbSet<Coach> Coaches { get; set; }
+    public DbSet<Game> Games { get; set; }
+    public DbSet<GameAttendance> Game_attendences { get; set; }
+    public DbSet<Player> Players { get; set; }
+    public DbSet<Practice> Practices { get; set; }
+    public DbSet<PracticeAttendance> Practice_attendences { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<User> Users { get; set; }
 }

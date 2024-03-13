a8657e37ef58664926037477a386160f2abe8f23
Kristian Walin <Kristianw@live.no>
2024-03-13 13:11:03 +0100

    Added Controller

---
+++ src/CoachConnect.API/Controllers/TeamsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoachConnect.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TeamsController : ControllerBase
+{
+    // GET: api/<TeamsController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<TeamsController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<TeamsController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<TeamsController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<TeamsController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}

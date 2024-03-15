9ec20836414fe8e6bcff644e1c336bb20a29a256
KiwiKetil <122778291+KiwiKetil@users.noreply.github.com>
2024-03-15 13:09:43 +0100

    Add validation to user and coach (#18)
    * added Fluentvalidation for CoachDTO, CoachRegistrationDTO, UserDTO, UserRegistrationDTO
    
    * added Fluentvalidation for CoachDTO, CoachRegistrationDTO, UserDTO, UserRegistrationDTO v2

---
+++ src/CoachConnect.BusinessLayer/Validators/UserDTOValidator.cs
@@ -0,0 +1,27 @@
+﻿using CoachConnect.BusinessLayer.DTOs;
+using CoachConnect.DataAccess.Entities;
+using FluentValidation;
+
+namespace CoachConnect.BusinessLayer.Validators;
+
+public class UserDTOValidator : AbstractValidator<UserDTO>
+{
+    public UserDTOValidator() // må endres ettterhvert som vi oppdaterer DTO
+    {    
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("FirstName can not be null")
+            .MaximumLength(16).WithMessage("FirstName limit exceeded (max 16 characters)");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("LastName can not be null")
+            .MaximumLength(16).WithMessage("LastName limit exceeded (max 16 characters)");
+
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("Phonenumber can not be null")
+            .Length(8).WithMessage("Phonenumber must be 8 digits");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email must be included")
+            .EmailAddress().WithMessage("Email must be valid");    
+    }
+}

b232b90625eccc83613b95f2953105bba69aff3b
KiwiKetil <122778291+KiwiKetil@users.noreply.github.com>
2024-04-09 11:11:56 +0200

    Add jwt (#41)
    * added logindto working on authorization
    
    * possibly jwt working , added [Authorize] to UsersController
    
    * added Login.cs so User and Coach have in common return value

---
+++ src/CoachConnect.BusinessLayer/DTOs/LoginDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace CoachConnect.BusinessLayer.DTOs;
+
+public class LoginDTO
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}

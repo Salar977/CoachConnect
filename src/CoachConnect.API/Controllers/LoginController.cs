22c83cfd6df4cb456bcbf6ac70edb33e0c818d9c
KiwiKetil <ketilsveberg@gmail.com>
2024-04-11 14:28:04 +0200

    tried fixing to be able to reach Roles via Users example: Users.Roles - no luck

--- src/CoachConnect.API/Controllers/LoginController.cs
+++ src/CoachConnect.API/Controllers/LoginController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace JWTAuthentication.Controllers;
@@ -46,14 +47,41 @@ public class LoginController : Controller
         var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
         var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-        var token = new JwtSecurityToken(_config["Jwt:Issuer"],
-          _config["Jwt:Issuer"],
-          null,
-          expires: DateTime.Now.AddMinutes(120),
-          signingCredentials: credentials);
+        var claims = new List<Claim>();
+    
+
+        // Add UserId claim if the userOrCoachInfo is a User or Coach
+        if (userOrCoachInfo is User user)
+        {
+            claims.Add(new Claim("UserId", user.Id.ToString()));
+            claims.Add(new Claim("UserName", user.Email.ToString()));
+            //foreach (var role in user.Roles)
+            //{
+            //    claims.Add(new Claim("Role", role));
+            //}
+        }
+        else if (userOrCoachInfo is Coach coach)
+        {
+            claims.Add(new Claim("UserId", coach.Id.ToString()));
+            claims.Add(new Claim("UserName", coach.Id.ToString()));
+            //foreach (var role in coach.Roles)
+            //{
+            //    claims.Add(new Claim("Role", role));
+            //}
+        }
+
+        // admin claims missing
+
+        var token = new JwtSecurityToken(
+            _config["Jwt:Issuer"],
+            _config["Jwt:Issuer"],
+            claims,
+            expires: DateTime.Now.AddMinutes(120),
+            signingCredentials: credentials);
 
         return new JwtSecurityTokenHandler().WriteToken(token);
     }
+
 
     private Login? AuthenticateUser(LoginDTO loginDto)
     {

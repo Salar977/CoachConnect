63f3c361831fe2d33ab7502f4d9788ef9b6dd70d
KiwiKetil <ketilsveberg@gmail.com>
2024-04-11 13:29:29 +0200

    needed for commit

--- src/CoachConnect.DataAccess/Entities/JwtUserRole.cs
+++ src/CoachConnect.DataAccess/Entities/JwtUserRole.cs
@@ -6,31 +6,22 @@ using static CoachConnect.DataAccess.Entities.JwtRole;
 namespace CoachConnect.DataAccess.Entities;
 
 public readonly record struct JwtUserRoleId(Guid jwtUserRoleId)
-<<<<<<< HEAD
+
 {
     public static JwtUserRoleId NewId => new JwtUserRoleId(Guid.NewGuid());
     public static JwtUserRoleId Empty => new JwtUserRoleId(Guid.Empty);
 };
+
 public class JwtUserRole 
 {
-=======
-{
-    public static JwtUserRoleId NewId => new JwtUserRoleId(Guid.NewGuid());
-    public static JwtUserRoleId Empty => new JwtUserRoleId(Guid.Empty);
-};
-public class JwtUserRole 
-{
->>>>>>> main
+
     [Key]
     public JwtUserRoleId Id { get; set; }
 
     public Guid UserOrCoachId { get; set; }
-<<<<<<< HEAD
 
-    [ForeignKey(nameof(RoleId))]
-=======
-       
->>>>>>> main
+    [ForeignKey(nameof(RoleId))]       
+
     public int RoleId { get; set; }
 
     public JwtRole? Role { get; set; }

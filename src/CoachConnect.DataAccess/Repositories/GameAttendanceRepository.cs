f1403fd727c03d8b729683e8b3fb701d8a6ace0c
KiwiKetil <ketilsveberg@gmail.com>
2024-04-16 13:33:55 +0200

    added player first,lastname, opponent and Gametime in dto

--- src/CoachConnect.DataAccess/Repositories/GameAttendanceRepository.cs
+++ src/CoachConnect.DataAccess/Repositories/GameAttendanceRepository.cs
@@ -61,6 +61,9 @@ public class GameAttendanceRepository : IGameAttendanceRepository
         var skipNumber = (gameAttendanceQuery.PageNumber - 1) * gameAttendanceQuery.PageSize;
 
         return await gameAttendances
+            .Include(g => g.Player)
+            .Include(g => g.Game)
+            .Include(g => g.Game)
             .Skip(skipNumber)
             .Take(gameAttendanceQuery.PageSize)
             .ToListAsync();
@@ -68,10 +71,17 @@ public class GameAttendanceRepository : IGameAttendanceRepository
 
     public async Task<GameAttendance?> GetByIdAsync(GameAttendanceId id)
     {
-        _logger.LogDebug("Getting gameAttendance by id: {id} from db", id);
+    _logger.LogDebug("Getting gameAttendance by id: {id} from db", id);
 
-        return await _dbContext.Game_attendences.FindAsync(id);
+    var gameAttendance = await _dbContext.Game_attendences
+        .Include(g => g.Player)
+        .Include(g => g.Game)
+        .FirstOrDefaultAsync(g => g.Id == id);
+
+    return gameAttendance; 
     }
+
+
 
     public async Task<GameAttendance?> RegisterGameAttendanceAsync(GameAttendance gameAttendance)
     {

5516edc8eda9117cc3fce54a519e1d7e23e87ce3
Salar Amir <salar831@gmail.com>
2024-04-24 11:33:01 +0200

    added environment variables for connection string for security

--- src/CoachConnect.DataAccess/DependencyInjection.cs
+++ src/CoachConnect.DataAccess/DependencyInjection.cs
@@ -4,6 +4,7 @@ using CoachConnect.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
 
 namespace CoachConnect.DataAccess;
 
@@ -11,9 +12,15 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        // Database Configuration
         services.AddDbContext<CoachConnectDbContext>(options =>
         {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            connectionString = connectionString?
+                .Replace("{COACH_CONNECT_USERNAME}", Environment.GetEnvironmentVariable("COACH_CONNECT_USERNAME"))
+                .Replace("{COACH_CONNECT_PASSWORD}", Environment.GetEnvironmentVariable("COACH_CONNECT_PASSWORD"));
+
             options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         });
 

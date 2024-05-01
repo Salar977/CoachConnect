b1e7df5ebead479bb3099e008d55470391b5299f
KiwiKetil <ketilsveberg@gmail.com>
2024-05-01 13:44:09 +0200

    reinstated email in user and coachquery helper class

--- src/CoachConnect.Shared/Helpers/CoachQuery.cs
+++ src/CoachConnect.Shared/Helpers/CoachQuery.cs
@@ -5,6 +5,8 @@ public class CoachQuery
     public string? FirstName { get; set; } = null;
     public string? LastName { get; set; } = null;
     public string? PhoneNumber { get; set; } = null;
+    public string? Email { get; set; } = null;
+
     public string? SortBy { get; set; } = null;
     public bool IsDescending { get; set; } = false;
     public int PageNumber { get; set; } = 1;

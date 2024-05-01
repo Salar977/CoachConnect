36050431d3e6fde203687035afce5fd7f7f8f9fa
KiwiKetil <ketilsveberg@gmail.com>
2024-05-01 13:42:18 +0200

    contined work on testing moved tests to controllertests

--- tests/CoachConnect.IntegrationTests/Controllers/UsersControllerTests.cs
+++ tests/CoachConnect.IntegrationTests/Controllers/UsersControllerTests.cs
@@ -1,4 +1,6 @@
 ﻿using CoachConnect.BusinessLayer.DTOs;
+using CoachConnect.DataAccess.Entities;
+using CoachConnect.Shared.Helpers;
 using System.Net;
 using System.Text;
 
@@ -12,29 +14,148 @@ public class UsersControllerTests : BaseIntegrationTests
     }
 
     [Fact]
-    public async Task GetUsersAsync_DefaultPageSize_ReturnData() 
+    public async Task Get_Users_Async_DefaultPageSize_Empty_Query_Return_Status_OK_And_Users() 
     {
         // arrange
 
         //LoginDTO dto = new LoginDTO { Username = "quyen123@hotmail.com", Password = "Q1yenAdmin#" };
         //var jsonLoginDto = System.Text.Json.JsonSerializer.Serialize<LoginDTO>(dto);
 
-        //// act
-        //// login > return token
-        //// add token to http-header for Client
-        ////Client.DefaultRequestHeaders("Bearer", "...")
+        var userQuery = new UserQuery();
+
+        // act
+
         //StringContent content = new StringContent(jsonLoginDto, System.Text.Encoding.UTF8, "application/json");
         //var loginResult = await Client!.PostAsync("api/v1/login", content);
-        //// token
         //var token = await loginResult.Content.ReadAsStringAsync();
-
-        //// setter token
         //Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
 
         var response = await Client!.GetAsync("api/v1/users");
+        var userDtos = await UserService!.GetAllAsync(userQuery);
 
         // assert
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(userDtos);
+        Assert.Equal(10, userDtos.ToList().Count);
+    }
+
+    [Fact]
+    public async Task Get_Users_By_LastName_Using_Query_Async_DefaultSize_10_Return_Users()
+    {
+        // arrange
+
+        //LoginDTO dto = new LoginDTO { Username = "quyen123@hotmail.com", Password = "Q1yenAdmin#" };
+        //var jsonLoginDto = System.Text.Json.JsonSerializer.Serialize<LoginDTO>(dto);
+
+        var userQuery = new UserQuery { LastName = "Andersen" };
+
+        // act
+
+        //StringContent content = new StringContent(jsonLoginDto, System.Text.Encoding.UTF8, "application/json");
+        //var loginResult = await Client!.PostAsync("api/v1/login", content);
+        //var token = await loginResult.Content.ReadAsStringAsync();
+        //Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        var response = await Client!.GetAsync("/api/v1/users?LastName=Andersen");
+        var userDtos = await UserService!.GetAllAsync(userQuery);
+
+        // assert
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(userDtos);
+        Assert.Equal(2, userDtos.ToList().Count);
+    }
+
+    [Fact]
+    public async Task Get_User_By_Id_Async_Returns_Status_OK_And_User()
+    {
+        // arrange
+
+        //LoginDTO dto = new LoginDTO { Username = "quyen123@hotmail.com", Password = "Q1yenAdmin#" };
+        //var jsonLoginDto = System.Text.Json.JsonSerializer.Serialize<LoginDTO>(dto);
+
+        var guid = new Guid("22222222-2222-2222-2222-222222222222");
+        var userId = new UserId(new Guid("22222222-2222-2222-2222-222222222222"));
+
+        User user = new()
+        {
+            Id = userId,
+            FirstName = "Maria",
+            LastName = "Mariah",
+            PhoneNumber = "99999999",
+            Email = "mariah@yahoo.com",
+            HashedPassword = "$2a$11$2nb9L2C0b8QLyU5xRdpqtu7/Qw89vF7aLl0yWQ/dnMZ8M2N/cDBhK",
+            Salt = "$2a$11$2nb9L2C0b8QLyU5xRdpqtu",
+            Created = new DateTime(2024, 02, 22, 19, 54, 51),
+            Updated = new DateTime(2024, 02, 22, 19, 54, 51),
+
+        };
+
+        // act
+
+        //StringContent content = new StringContent(jsonLoginDto, System.Text.Encoding.UTF8, "application/json");
+        //var loginResult = await Client!.PostAsync("api/v1/login", content);
+        //var token = await loginResult.Content.ReadAsStringAsync();
+        //Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        var response = await Client!.GetAsync("api/v1/users/22222222-2222-2222-2222-222222222222");
+        var userDto = await UserService!.GetByIdAsync(guid);
+
+        // assert
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(userDto);
+        Assert.Equal(user.Id, userDto.Id);
+        Assert.Equal(user.FirstName, userDto.FirstName);
+        Assert.Equal(user.LastName, userDto.LastName);
+        Assert.Equal(user.PhoneNumber, userDto.PhoneNumber);
+        Assert.Equal(user.Email, userDto.Email);
+    }
+
+    [Fact]
+    public async Task Get_User_By_Email_Async_Return_User_Data()
+    {
+        // arrange
+
+        //LoginDTO dto = new LoginDTO { Username = "quyen123@hotmail.com", Password = "Q1yenAdmin#" };
+        //var jsonLoginDto = System.Text.Json.JsonSerializer.Serialize<LoginDTO>(dto);
+
+        var userId = new UserId(new Guid("22222222-2222-2222-2222-222222222222"));
+        var email = "mariah@yahoo.com";
+
+        User user = new()
+        {
+            Id = userId,
+            FirstName = "Maria",
+            LastName = "Mariah",
+            PhoneNumber = "99999999",
+            Email = email,
+            HashedPassword = "$2a$11$2nb9L2C0b8QLyU5xRdpqtu7/Qw89vF7aLl0yWQ/dnMZ8M2N/cDBhK",
+            Salt = "$2a$11$2nb9L2C0b8QLyU5xRdpqtu",
+            Created = new DateTime(2024, 02, 22, 19, 54, 51),
+            Updated = new DateTime(2024, 02, 22, 19, 54, 51),
+
+        };
+
+        // act
+
+        //StringContent content = new StringContent(jsonLoginDto, System.Text.Encoding.UTF8, "application/json");
+        //var loginResult = await Client!.PostAsync("api/v1/login", content);
+        //var token = await loginResult.Content.ReadAsStringAsync();
+        //Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        var response = await Client!.GetAsync("api/v1/users/mariah@yahoo.com");
+        var userDto = await UserService!.GetByEmailAsync(email);
+
+        // assert
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(userDto);
+        Assert.Equal(user.Id, userDto.Id);
+        Assert.Equal(user.FirstName, userDto.FirstName);
+        Assert.Equal(user.LastName, userDto.LastName);
+        Assert.Equal(user.PhoneNumber, userDto.PhoneNumber);
+        Assert.Equal(user.Email, userDto.Email);
     }
 }   
--- tests/CoachConnect.IntegrationTests/Services/UserServiceTests.cs
+++ tests/CoachConnect.IntegrationTests/Services/UserServiceTests.cs
@@ -6,59 +6,106 @@ namespace CoachConnect.IntegrationTests.Services;
 
 public class UserServiceTests : BaseIntegrationTests
 {
-
     public UserServiceTests(CoachConnectWebAppFactory factory) 
         : base(factory)
     {        
     }
 
-    [Fact]
-    public async Task GetUsersAsync_DefaultSize_10_Return_10_Users() 
-    {
-        // arrange
-        // test data som vi skal sjekke resultatet mot!
-        var userQuery = new UserQuery();
+    //[Fact]
+    //public async Task Get_Users_Async_DefaultSize_10_Return_10_Users() 
+    //{
+    //    // arrange
 
-        // act
-        var userDtos = await UserService!.GetAllAsync(userQuery);
+    //    var userQuery = new UserQuery();
 
-        // assert
-        Assert.NotNull(userDtos);
-        Assert.Equal(10, userDtos.ToList().Count);
-    }
+    //    // act
 
-    [Fact]
-    public async Task GetUserByIdAsync_Return_User()
-    {
-        // arrange
+    //    var userDtos = await UserService!.GetAllAsync(userQuery);
 
-        var userId = new UserId(new Guid("22222222-2222-2222-2222-222222222222"));
+    //    // assert
+    //    Assert.NotNull(userDtos);
+    //    Assert.Equal(10, userDtos.ToList().Count);
+    //}
 
-        User user = new()
-        {
-            Id = userId,
-            FirstName = "Maria",
-            LastName = "Mariah",
-            PhoneNumber = "99999999",
-            Email = "mariah@yahoo.com",
-            HashedPassword = "$2a$11$2nb9L2C0b8QLyU5xRdpqtu7/Qw89vF7aLl0yWQ/dnMZ8M2N/cDBhK",
-            Salt = "$2a$11$2nb9L2C0b8QLyU5xRdpqtu",
-            Created = new DateTime(2024, 02, 22, 19, 54, 51),
-            Updated = new DateTime(2024, 02, 22, 19, 54, 51),
+    //[Fact]
+    //public async Task Get_Users_By_LastName_Async_DefaultSize_10_Return_Users()
+    //{
+    //    // arrange
+    //    // test data som vi skal sjekke resultatet mot!
+    //    var userQuery = new UserQuery { LastName = "Andersen" };
+
+    //    // act
+    //    var userDtos = await UserService!.GetAllAsync(userQuery);
+
+    //    // assert
+    //    Assert.NotNull(userDtos);
+    //    Assert.Equal(2, userDtos.ToList().Count);
+    //}
+
+    //[Fact]
+    //public async Task Get_User_By_Id_Async_Return_User()
+    //{
+    //    // arrange
+
+    //    var userId = new UserId(new Guid("22222222-2222-2222-2222-222222222222"));
+
+    //    User user = new()
+    //    {
+    //        Id = userId,
+    //        FirstName = "Maria",
+    //        LastName = "Mariah",
+    //        PhoneNumber = "99999999",
+    //        Email = "mariah@yahoo.com",
+    //        HashedPassword = "$2a$11$2nb9L2C0b8QLyU5xRdpqtu7/Qw89vF7aLl0yWQ/dnMZ8M2N/cDBhK",
+    //        Salt = "$2a$11$2nb9L2C0b8QLyU5xRdpqtu",
+    //        Created = new DateTime(2024, 02, 22, 19, 54, 51),
+    //        Updated = new DateTime(2024, 02, 22, 19, 54, 51),
             
-        };
-        // testdata som vi skal sjekke resultatet mot
-        var guid = new Guid("22222222-2222-2222-2222-222222222222");
-        // act
-        var userDto = await UserService!.GetByIdAsync(guid);
+    //    };
+    //    var guid = new Guid("22222222-2222-2222-2222-222222222222");
+    //    // act
+    //    var userDto = await UserService!.GetByIdAsync(guid);
 
-        // assert
-        Assert.NotNull(userDto);
-        Assert.Equal(user.Id, userDto.Id);
-        Assert.Equal(user.FirstName, userDto.FirstName);
-        Assert.Equal(user.LastName, userDto.LastName);
-        Assert.Equal(user.PhoneNumber, userDto.PhoneNumber);
-        Assert.Equal(user.Email, userDto.Email);
+    //    // assert
+    //    Assert.NotNull(userDto);
+    //    Assert.Equal(user.Id, userDto.Id);
+    //    Assert.Equal(user.FirstName, userDto.FirstName);
+    //    Assert.Equal(user.LastName, userDto.LastName);
+    //    Assert.Equal(user.PhoneNumber, userDto.PhoneNumber);
+    //    Assert.Equal(user.Email, userDto.Email);
         
-    }
+    //}
+
+    //[Fact]
+    //public async Task Get_User_By_Email_Async_Return_User_Data()
+    //{
+    //    // arrange
+    //    var userId = new UserId(new Guid("22222222-2222-2222-2222-222222222222"));
+    //    var email = "mariah@yahoo.com";
+
+    //    User user = new()
+    //    {
+    //        Id = userId,
+    //        FirstName = "Maria",
+    //        LastName = "Mariah",
+    //        PhoneNumber = "99999999",
+    //        Email = email,
+    //        HashedPassword = "$2a$11$2nb9L2C0b8QLyU5xRdpqtu7/Qw89vF7aLl0yWQ/dnMZ8M2N/cDBhK",
+    //        Salt = "$2a$11$2nb9L2C0b8QLyU5xRdpqtu",
+    //        Created = new DateTime(2024, 02, 22, 19, 54, 51),
+    //        Updated = new DateTime(2024, 02, 22, 19, 54, 51),
+
+    //    };
+    //    // testdata som vi skal sjekke resultatet mot
+    //    // act
+    //    var userDto = await UserService!.GetByEmailAsync(email);
+
+    //    // assert
+    //    Assert.NotNull(userDto);
+    //    Assert.Equal(user.Id, userDto.Id);
+    //    Assert.Equal(user.FirstName, userDto.FirstName);
+    //    Assert.Equal(user.LastName, userDto.LastName);
+    //    Assert.Equal(user.PhoneNumber, userDto.PhoneNumber);
+    //    Assert.Equal(user.Email, userDto.Email);
+    //}
 }
